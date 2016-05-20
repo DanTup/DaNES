9f6db666b3751990d778ee1fb962afa869ce9174
Danny Tuppeny <danny@tuppeny.com>
2016-05-20 11:13:40 +0100

    Added classes for CPU/Memory.

---
+++ DaNES.Emulation/Cpu.cs
@@ -0,0 +1,19 @@
+﻿namespace DanTup.DaNES.Emulation
+{
+	public class Cpu
+	{
+		byte Accumulator, XRegister, YRegister;
+		ushort ProgramCounter, StackPointer;
+		bool Negative, Overflow, Interrupted, DecimalMode, InterruptsEnabled, ZeroResult, Carry;
+		Memory Ram;
+
+		public Cpu()
+		{
+			Ram = new Memory(0x10000);
+		}
+
+		public void Run()
+		{
+		}
+	}
+}
---
+++ DaNES.Emulation/Memory.cs
@@ -0,0 +1,16 @@
+﻿namespace DanTup.DaNES.Emulation
+{
+	public class Memory
+	{
+		byte[] memory;
+
+		public Memory(int size)
+		{
+			memory = new byte[size];
+		}
+
+		public byte Read(int address) => memory[address];
+
+		public void Write(int address, byte value) => memory[address] = value;
+	}
+}
