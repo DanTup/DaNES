03d9493d935c3787decfdc4b3d98bb9e458b7fe8
Danny Tuppeny <danny@tuppeny.com>
2016-05-20 11:34:19 +0100

    Created a logging CPU for comparing to known good nestest output.

--- DaNES.Emulation.Tests/CpuTests/NesTest.cs
+++ DaNES.Emulation.Tests/CpuTests/NesTest.cs
@@ -1,19 +1,27 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Text;
 using Xunit;
 
 namespace DanTup.DaNES.Emulation.Tests.CpuTests
 {
 	public class NesTest : CpuTests
 	{
+		const string ActualLogFile = "../../Logs/nestest.actual.log";
+		const string ExpectedLogFile = "../../Logs/nestest.expected.log";
 		const string RomFile = "../../Roms/nestest.nes";
 
+		{
 		[Fact]
 		public void RunTests()
 		{
 			var program = new ArraySegment<byte>(File.ReadAllBytes(RomFile), 0x0010, 0x4000).ToArray();
 			Run(program);
+
+			{
+			}
 		}
 	}
 }
