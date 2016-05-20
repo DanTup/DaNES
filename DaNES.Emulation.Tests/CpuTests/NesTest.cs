4cfe6634bf95b0f305164beaef681f14192021ef
Danny Tuppeny <danny@tuppeny.com>
2016-05-20 11:32:13 +0100

    Add a test that is able to load and execute NESTest.

---
+++ DaNES.Emulation.Tests/CpuTests/NesTest.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using Xunit;
+
+namespace DanTup.DaNES.Emulation.Tests.CpuTests
+{
+	public class NesTest : CpuTests
+	{
+		const string RomFile = "../../Roms/nestest.nes";
+
+		[Fact]
+		public void RunTests()
+		{
+			var program = new ArraySegment<byte>(File.ReadAllBytes(RomFile), 0x0010, 0x4000).ToArray();
+			Run(program);
+		}
+	}
+}
