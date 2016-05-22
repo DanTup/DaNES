fa6c4bd13d63163cb1cc8a48f37c51165f388713
Danny Tuppeny <danny@tuppeny.com>
2016-05-22 16:45:44 +0100

    Extract common stuff for testing.

--- DaNES.Emulation.Tests.FSharp/LdaTests.fs
+++ DaNES.Emulation.Tests.FSharp/LdaTests.fs
@@ -2,22 +2,6 @@
 
 open System
 open Xunit
-open DanTup.DaNES.Emulation
-
-// Helper to run a program and return the CPU state.
-let run bytes =
-    let nes = Nes()
-    nes.LoadProgram(bytes |> List.toArray)
-    nes.Run()
-    nes.Cpu
-
-// Test helpers
-let shouldBe (expected : 'a) (actual : 'a) =
-    Assert.Equal<'a>(expected, actual)
-
-// OpCodes
-[<Literal>]
-let op_lda_immediate = 0xA9uy
 
 [<Theory>]
 [<InlineData(0, true, false)>]
@@ -27,7 +11,7 @@ let op_lda_immediate = 0xA9uy
 [<InlineData(129, false, true)>]
 [<InlineData(255, false, true)>]
 let lda_immediate value_to_load expect_zero expect_negative =
-    let state = run [ op_lda_immediate; value_to_load ]
+    let state = run [ byte Opcode.LDA_IMD; value_to_load ]
     state.Accumulator |> shouldBe value_to_load
     state.ZeroResult |> shouldBe expect_zero
     state.Negative |> shouldBe expect_negative
---
+++ DaNES.Emulation.Tests.FSharp/TestHelpers.fs
@@ -0,0 +1,18 @@
+﻿[<AutoOpen>]
+module TestHelpers
+
+open DanTup.DaNES.Emulation
+open Xunit
+
+type Opcode = Cpu6502.OpCode
+
+// Test asserts
+let shouldBe (expected : 'a) (actual : 'a) =
+    Assert.Equal<'a>(expected, actual)
+
+// Helper to run a program and return the CPU state.
+let run bytes =
+    let nes = Nes()
+    nes.LoadProgram(bytes |> List.toArray)
+    nes.Run()
+    nes.Cpu
